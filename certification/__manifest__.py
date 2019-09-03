ddeb5d8f5ce62febfa833812a0855095e959d655
Veysel Gunduzalp <vgunduzalp@gmail.com>
2019-09-03 05:29:33 -0700

    added certificate

---
+++ certification/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Certification",
+    "summary": "sdsadsad",
+    'version': '12.0.1.0.0',
+    "category":"Certification ",
+    "depends": [
+        'base'
+    ],
+    "data": [ 'security/ir.model.access.csv',
+              'views/certification_view.xml',
+              'views/standard_view.xml',
+              'views/res_partner_view.xml'
+    ],
+    'development_status':'Beta',
+    'maintainers':['ceeficent'],
+}
---
+++ employeeid/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "EmployeeTest",
+    "summary": "EmployeeTest",
+    'version': '1.0',
+    "category":"Certification",
+    "depends": [
+        'base','hr'
+    ],
+    "data": [ 'security/ir.model.access.csv',
+              'views/employee_info_view.xml',
+    ],
+    'development_status':'Beta',
+    'maintainers':['ceeficent'],
+}
